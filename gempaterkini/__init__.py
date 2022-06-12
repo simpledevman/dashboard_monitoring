ebcb4d160ede8406feeeb9ccc37fc4bf717d1a0e
simpledevman <dev.heeroe@gmail.com>
2022-06-12 16:24:32 +0800

    initial commit

---
+++ gempaterkini/__init__.py
@@ -0,0 +1,11 @@
+def ekstraksi_data():
+    """
+    tangal: 12 Juni 2022
+    waktu: 10:25:26 WIB
+    skala: 4.7
+    kedalaman: 10 km
+    lokasi: 9.72 LS - 119.08 BT
+    Pusat gempa:  berada di laut 17 km Tenggara KODI-SUMBA BARAT DAYA
+    Dirasakan: (Skala MMI): II - III Tambolaka, II Waikabubak
+    :return:
+    """
