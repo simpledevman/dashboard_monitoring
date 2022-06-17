6f274d708de66088b49d79f94954886b36763c12
simpledevman <dev.heeroe@gmail.com>
2022-06-18 07:06:50 +0800

    mengambil data html

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -13,22 +13,32 @@ def ekstraksi_data():
     dirasakan: Dirasakan (Skala MMI): II - III Tambolaka, II Waikabubak
     :return:
     """
-    content = requests.get('https://bmkg.go.id')
-    soup = BeautifulSoup(content)
-    print(soup.prettify())
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html parser')
 
-    hasil = dict()
-    hasil['tanggal'] = '12 Juni 2022'
-    hasil['waktu'] = '10:25:26 WIB'
-    hasil['magnitudo'] = 4.7
-    hasil['kedalaman'] = '10 km'
-    hasil['lokasi'] = {'ls': 9.72, 'bt': 119.08}
-    hasil['pusat gempa'] = 'Pusat gempa berada di laut 17 km Tenggara KODI-SUMBA BARAT DAYA'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II - III Tambolaka, II Waikabubak'
 
-    return hasil
+
+
+        hasil = dict()
+        hasil['tanggal'] = '12 Juni 2022'
+        hasil['waktu'] = '10:25:26 WIB'
+        hasil['magnitudo'] = 4.7
+        hasil['kedalaman'] = '10 km'
+        hasil['lokasi'] = {'ls': 9.72, 'bt': 119.08}
+        hasil['pusat'] = 'Pusat gempa berada di laut 17 km Tenggara KODI-SUMBA BARAT DAYA'
+        hasil['dirasakan'] = 'Dirasakan (Skala MMI): II - III Tambolaka, II Waikabubak'
+        return hasil
+    else:
+        return None
 
 def tampilkan_data(result):
+    if result is None:
+        print('tidak bisa menemukan data')
+        return
     print('gempa terakhir berdasarkan BMKG')
     print(f"tanggal {result['tanggal']}")
     print(f"waktu {result['waktu']}")
@@ -36,3 +46,7 @@ def tampilkan_data(result):
     print(f"kedalaman {result['kedalaman']}")
     print(f"lokasi: LS= {result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
     print(f"dirasakan {result['dirasakan']}")
+
+
+if __name__=='__main__':
+    print('ini adalah package gempa terkini')
