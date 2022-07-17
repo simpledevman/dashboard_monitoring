f90ac9a6b6a7d9d5f52cd9f2fb0ebb61c5772077
simpledevman <dev.heeroe@gmail.com>
2022-07-18 07:11:29 +0800

    mengambil data html

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -18,15 +18,27 @@ def ekstraksi_data():
     except Exception:
         return None
     if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html parser')
+        soup = BeautifulSoup(content.text, 'html.parser')
 
+        result = soup.find('span', {'class': 'waktu'})
+        result = result.text.split(', ')
+        tanggal = result[0]
+        waktu = result[1]
 
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        i = 0
+        for res in result:
+            print(i, res)
+            i = i + 1
+
+        magnitudo = 0
 
 
         hasil = dict()
-        hasil['tanggal'] = '12 Juni 2022'
-        hasil['waktu'] = '10:25:26 WIB'
-        hasil['magnitudo'] = 4.7
+        hasil['tanggal'] = tanggal #'12 Juni 2022'
+        hasil['waktu'] = waktu #'10:25:26 WIB'
+        hasil['magnitudo'] = magnitudo #4.7
         hasil['kedalaman'] = '10 km'
         hasil['lokasi'] = {'ls': 9.72, 'bt': 119.08}
         hasil['pusat'] = 'Pusat gempa berada di laut 17 km Tenggara KODI-SUMBA BARAT DAYA'
@@ -48,5 +60,7 @@ def tampilkan_data(result):
     print(f"dirasakan {result['dirasakan']}")
 
 
-if __name__=='__main__':
+if __name__=='__main__': # aplikasi gempa terkini
     print('ini adalah package gempa terkini')
+
+
